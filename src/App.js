bed999b5aa1609a1ea3713c3259b7680a09289d2
Nick Stanford <nick@reach.sh>
2023-02-09 19:07:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+//import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+//import '@fontsource/roboto/500.css';
+//import '@fontsource/roboto/700.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+
 
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const mealPlan = 'Meal Planner';
+  const recipes = 'Recipes';
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Box display="flex" flexGrow={1}>
+            Pantry Pal
+          </Box>
+            <Button 
+              variant="outline" 
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >Recipes</Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Broccoli Stir-fry</MenuItem>
+              <MenuItem onClick={handleClose}>Jambalaya</MenuItem>
+              <MenuItem onClick={handleClose}>Mexican Quinoa</MenuItem>
+            </Menu>
+            <Button variant="outline">Blog</Button>
+            <Button variant="outline">Pantry Staples</Button>
+            <Button variant="outline">Meal Planner</Button>
+            <Button variant="outline">Recipe Creator</Button>
+            <Avatar></Avatar>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 }
-
 export default App;
